b4ea5fc51f80c6a74041e72e47f89a51a0fc052b
bodywizard <david@bodywizardmedicine.com>
2022-02-10 16:47:19 -0600

    upgrade app to laravel 9

--- resources/views/layouts/menus/menu-bar.blade.php
+++ resources/views/layouts/menus/menu-bar.blade.php
@@ -1,35 +1,32 @@
-<?php 
-    // // include_once app_path("/php/functions.php");
-    $menuJson = json_decode(file_get_contents(app_path("/json/menu-data.json")),true);
+<?php
+// // include_once app_path("/php/functions.php");
+$menuJson = json_decode(file_get_contents(app_path('/json/menu-data.json')), true);
 
-    $allTabs = $menuJson[$menu_data];
-    $scroll = "";
-    $items = isset($items) ? $items : [];
-    $type = isset($type) ? $type : 'portal';
+$allTabs = $menuJson[$menu_data];
+$scroll = '';
+$items = isset($items) ? $items : [];
+$type = isset($type) ? $type : 'portal';
 
-    if ($type == 'site'){
-        $classes = "menuBar website siteMenu";
-        $target = "window";
-    }elseif ($type == 'portal'){
-        $classes = "menuBar portal wrapper";
-        $target = "#".$menu_name."Target";
-    }elseif ($type == 'scroll'){
-        $classes = "menuBar website";
-        $target = "window";
-        $scroll = "data-mode='scroll' ";
-    }
+if ($type == 'site') {
+    $classes = 'menuBar website siteMenu';
+    $target = 'window';
+} elseif ($type == 'portal') {
+    $classes = 'menuBar portal wrapper';
+    $target = '#' . $menu_name . 'Target';
+} elseif ($type == 'scroll') {
+    $classes = 'menuBar website';
+    $target = 'window';
+    $scroll = "data-mode='scroll' ";
+}
 ?>
 
 
-<div class="{{ $classes }}"
-     id="{{ $menu_name }}" 
-     data-target="{{ $target }}"
-     {{ $scroll }}>
-    @if ($type == 'site')
-        @include ('layouts.menus.menu-item',array_merge($menuJson['MobileMenu']['MobilePlaceholder'],['name'=>'MobileMenu']))
-    @endif
+<div class="{{ $classes }}" id="{{ $menu_name }}" data-target="{{ $target }}" {{ $scroll }}>
     @forelse ($items as $name)
-        @include ('layouts.menus.menu-item',array_merge($allTabs[$name],compact('name')))
+        <?php $vars = array_merge($allTabs[$name], compact('name'));
+        // logger('menu-item data', $vars);
+        ?>
+        @include ('layouts.menus.menu-item',$vars)
     @empty
         @foreach ($allTabs as $name)
             @include ('layouts.menus.menu-item',array_merge($allTabs[$name],compact('name')))
@@ -41,6 +38,6 @@
     <div id="{{ $menu_name }}Target" class='loadTarget'> </div>
 @endif
 
-<?php 
+<?php
 unset($items);
 ?>
--- resources/views/layouts/menus/menu-item.blade.php
+++ resources/views/layouts/menus/menu-item.blade.php
@@ -1,24 +1,31 @@
-<?php 
-	$uri = !$dropdown ? $uri : null;
-	$divide = ($name == 'divide') ? " divide" : "";
-	$modal = isset($modal) && $modal ? 'true' : 'false';
+<?php
+if (!isset($dropdown)) {
+    $dropdown = false;
+}
+if (!isset($text)) {
+    $text = '';
+}
+$uri = !$dropdown && isset($uri) ? $uri : null;
+$divide = $name == 'divide' ? ' divide' : '';
+$modal = isset($modal) && $modal ? 'true' : 'false';
 ?>
 @if ($name == 'notifications')
-	@include ('layouts.notifications')
+    @include ('layouts.notifications')
 @else
-	<div id="{{$name}}" class="tab{{$divide}}" data-uri="{{$uri}}" data-modal="{{$modal}}">
-		@if (isset($image_url))
-			<img class="title" src="{{$image_url}}">
-		@else
-			<div class="title" data-uri="{{$uri}}">{{$text}}</div>
-		@endif
-		@if ($dropdown)
-		<?php unset($image_url); ?>		
-		<div class="dropdown">
-			@foreach ($dropdown as $name => $info)
-				@include('layouts.menus.menu-item',$info)
-			@endforeach		
-		</div>
-		@endif
-	</div>
+    <div id="{{ $name }}" class="tab {{ $divide }}" data-uri="{{ $uri }}"
+        data-modal="{{ $modal }}">
+        @if (isset($image_url))
+            <img class="title" src="{{ $image_url }}">
+        @else
+            <div class="title" data-uri="{{ $uri }}">{{ $text }}</div>
+        @endif
+        @if ($dropdown)
+            <?php unset($image_url); ?>
+            <div class="dropdown">
+                @foreach ($dropdown as $name => $info)
+                    @include('layouts.menus.menu-item',$info)
+                @endforeach
+            </div>
+        @endif
+    </div>
 @endif
